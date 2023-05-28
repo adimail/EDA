d2a38c4991d3687c0e40f2723d1536845e4b80d7
Aditya Godse <adimail2404@gmail.com>
2023-05-28 15:10:56 +0530

    Update data_analysis.py

--- matlab/data_analysis.py
+++ matlab/data_analysis.py
@@ -17,15 +17,42 @@ warnings.filterwarnings("ignore")
 train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
 
-train_data.info()
-test_data.info()
+# train_data.info()
+# test_data.info()
 
 train_data.drop(["Cabin"], axis=1, inplace=True)
 test_data.drop(["Cabin"], axis=1, inplace=True)
 
-#Survived
+# Survived
 # sns.countplot(train_data.Survived)
 # plt.show()
 
-sns.countplot(train_data.Pclass)
+#Pclass
+# sns.countplot(train_data.Pclass)
+# plt.show()
+# sns.barplot(x="Pclass", y="Survived", data=train_data)
+# plt.show()
+
+# plt.hist(train_data.Age, edgecolor='black')
+# plt.xlabel('Age')
+# plt.ylabel('count')
+# plt.show()
+
+# sns.boxplot(x='Survived', y='Age', data=train_data)
+# plt.show()
+
+train_data.Ticket.head(10)
+
+train_data.Name.head(10)
+
+AgeMedian_by_titles = train_data.groupby('Title')['Age'].median()
+AgeMedian_by_titles
+
+# for title in AgeMedian_by_titles.index:
+#     train_data['Age'][(train_data.Age.isnull()) & (train_data.Title == title)] = AgeMedian_by_titles[title]
+#     test_data['Age'][(test_data.Age.isnull()) & (test_data.Title == title)] = AgeMedian_by_titles[title]
+
+train_data['AgeBin_5'] = pd.qcut(train_data['Age'], 5)
+test_data['AgeBin_5'] = pd.qcut(test_data['Age'], 5)
+sns.barplot(x='AgeBin_5', y='Survived', data=train_data)
 plt.show()
