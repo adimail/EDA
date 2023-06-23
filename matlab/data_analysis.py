716ba6aba16951a6469a19f8ac4c4503b405bb37
Aditya Godse <adimail2404@gmail.com>
2023-06-23 20:38:11 +0530

    Update data_analysis.py

--- matlab/data_analysis.py
+++ matlab/data_analysis.py
@@ -3,16 +3,6 @@ import os
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
-from sklearn import metrics
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.feature_selection import RFECV
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score, GridSearchCV
-from sklearn.preprocessing import LabelEncoder
-import warnings
-
-warnings.filterwarnings("ignore")
 
 train_data = pd.read_csv("train.csv")
 test_data = pd.read_csv("test.csv")
