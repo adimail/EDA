dcba2ac7cb64ad2a350a6367601ab2bc69453b66
Aditya <adimail2404@gmail.com>
2023-10-23 23:41:41 +0530

    EDA oct 2023

--- matlab/data_analysis.py
+++ matlab/data_analysis.py
@@ -1,16 +1,16 @@
-# import numpy as np
-# import os
-# # import pandas as pd
-# # import seaborn as sns
-# # from matplotlib import pyplot as plt
-# # from sklearn import metrics
-# # from sklearn.ensemble import RandomForestClassifier
-# # from sklearn.feature_selection import RFECV
-# # from sklearn.linear_model import LogisticRegression
-# # from sklearn.metrics import accuracy_score
-# # from sklearn.model_selection import cross_val_score, GridSearchCV
-# # from sklearn.preprocessing import LabelEncoder
-# import warnings
+import numpy as np
+import os
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import RFECV
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import cross_val_score, GridSearchCV
+from sklearn.preprocessing import LabelEncoder
+import warnings
 
 # warnings.filterwarnings("ignore")
 
